b4e3ef8474920f77ae9eb12ddad525a402967eb6
maith96 <threedaniel385@gmail.com>
2022-06-03 01:52:00 +0300

    python calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     if opr == '+':
-        print('{} + {} = {}'.format(a, b, add(a, b)))
+        print('{} + {} = {}\n'.format(a, b, add(a, b)))
     elif opr == '-':
         print('{} - {} = {}'.format(a, b, sub(a, b)))
     elif opr == '*':
