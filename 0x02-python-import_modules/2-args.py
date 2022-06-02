9734e3dbd56029ac9e35b3b414bcbe7aed553132
maith96 <threedaniel385@gmail.com>
2022-06-02 20:33:08 +0300

    prints comand line args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,11 @@ import sys
 if __name__ == '__main__':
     len = len(sys.argv)
     if len < 2:
-        print('0 argument.')
+        print('0 arguments.')
+    if len == 2:
+        print('{} argument:'.format(len - 1))
+        for i in range(1, (len)):
+            print('{}: {}'.format(i, sys.argv[i]))
     else:
         print('{} arguments:'.format(len - 1))
         for i in range(1, (len)):
