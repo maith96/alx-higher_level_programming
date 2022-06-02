6906bd8c240ced460e6f81804caf0ba467c47c2f
maith96 <threedaniel385@gmail.com>
2022-06-02 07:40:23 +0300

    python day 2

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-print(', '.join('{}{}'.format(i,j) for i in range(0, 10) for j in range(0,10) if j > i and j != i))
+print(', '.join('{}{}'.format(i, j) for i in range(0, 10) for j in range(0, 10)
+ if j > i and j != i))
            
